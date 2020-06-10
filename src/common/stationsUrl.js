6012d44ecc44404d25cbe785b2ecdf921aa5178e
Konstantin Baklaev <kbaklaev@gmail.com>
2020-06-10 09:07:41 +0500

    nrj to https

--- src/common/stationsUrl.js
+++ src/common/stationsUrl.js
@@ -4,6 +4,6 @@ export const stations = {
   [AVTO]: "http://den.101.ru:4000/ar_66_03",
   [DETI]: "http://den.101.ru:4000/det_66_01",
   [COMEDY]: "http://den.101.ru:4000/com_66_01",
-  [NRJ]: "http://den.101.ru:4000/nrj_66_04",
+  [NRJ]: "https://pub0201.101.ru/stream/reg/mp3/128/region_energy_60",
   [HUMOR]: "http://den.101.ru:4000/hu_66_01"
 }
