37934f6ac07b1cc8b39241ba68804acdd3c9c887
Konstantin Baklaev <kbaklaev@gmail.com>
2020-06-10 17:11:24 +0500

    avto and humor to https

--- src/common/stationsUrl.js
+++ src/common/stationsUrl.js
@@ -1,9 +1,9 @@
 import { AVTO, DETI, COMEDY, NRJ, HUMOR } from './var'
 
 export const stations = {
-  [AVTO]: "http://den.101.ru:4000/ar_66_03",
+  [AVTO]: "https://pub0101.101.ru:443/stream/reg/mp3/128/region_avto_53",
   [DETI]: "http://den.101.ru:4000/det_66_01",
   [COMEDY]: "http://den.101.ru:4000/com_66_01",
-  [NRJ]: "https://pub0201.101.ru/stream/reg/mp3/128/region_energy_60",
-  [HUMOR]: "http://den.101.ru:4000/hu_66_01"
+  [NRJ]: "https://pub0101.101.ru/stream/reg/mp3/128/region_energy_60",
+  [HUMOR]: "https://pub0101.101.ru:8000/stream/reg/mp3/128/region_humor_3"
 }
